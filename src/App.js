be9b9a5d3be136fbf4bae37e9b6ec84d8f6efbc9
carlos ortega <carlosalbertoortegac@gmail.com>
2022-01-17 20:50:58 -0500

    justify for content

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
 import Navigation from "./components/Navigation";
 import {useState,useEffect} from 'react';
 
+ 
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=a2ee356d&s=batman
 
 
 function App() {
 
-  const [personajes,setPersonajes]= useState([])
+  const [series,setSeries]= useState([])
 
-  const uri='https://kitsu.io/api/edge/trending/anime';
+  const uri='https://www.breakingbadapi.com/api/characters ';
 
-    function getCharacter(){
-      fetch(uri)
-      .then(res=>res.json())
-      .then(response=>{
-        const {data=[]}=response
-        console.log(data)
-        setPersonajes(data)
-      })
-    }
+      const obtenerDatos = async ()=>{
+        const data =await fetch(`${uri}`)
+        const characters= await data.json()
+        console.log(characters)
+        setSeries(characters)
+      }
 
 
     useEffect(()=>{
-      getCharacter()
+      obtenerDatos()
     },[])
 
 
   return (
     <>
-    
-      <Navigation/>
-      <div className="container mt-5">
-        <div className="row">
-          {personajes.map((personajes,index)=>(
-            <div key={index} className="col-lg-3 col">
-                <div className="card mt-5" style={{minwidth:'200px'}}>
-                  <img src={personajes.image} alt=''/>
-                  <div className="card-body">
-                    <h5 className="card-title">titles:{personajes.name}</h5>
-                    <hr/>
-                    <p className="text-dark">createdAt:{personajes.explanation}</p>
-                    <p className="text-dark"> popularityRank:{}</p>
-                    <p className="text-dark"> Id:{}</p>
+      <main>
+        <Navigation/>
+        <div className="container mt-5">
+          <div className="row">
+            {series.map((item)=>(
+              <div key={item} className="col-lg-3 col">
+                  <div className="card mt-5" style={{minwidth:'150px'}}>
+                    <img src={item.img} widt={100} heigth={100} alt=''/>
+                    <div className="card-body">
+                      <h5 className="card-title">titles: {item.name}</h5>
+                      <hr/>
+                      <p className="text-dark">Alternate Names: {item.species}</p>
+                      <p className="text-dark"> Place wherei lived: {item.house}</p>
+                      <p className="text-dark"> Category: {item.category}</p>
+                    </div>
                   </div>
-                </div>
-            </div>
-          ))}
-
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </main>
     </>
     
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,8 @@
 import React,{useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Navbar,Button, Container,Nav,Form,FormControl, Modal, ModalTitle, ModalBody,ModalFooter} from "react-bootstrap"
+import {Button,Form,Modal, ModalTitle, ModalBody,ModalFooter} from "react-bootstrap"
 import ModalHeader from 'react-bootstrap/esm/ModalHeader';
-
+import {BrowserRouter as Router,Link,Route,Switch} from "react-router-dom";
 
 
 const Navigation = () => {
@@ -14,43 +14,44 @@ const Navigation = () => {
     return (
 
         <>
-            <Navbar bg='dark' variant='dark' fixed='top'>
-                <Container>
-                   <Navbar.Brand  href='#logo'text-bg='white'>MarvelAPI</Navbar.Brand> 
-                   <Navbar.Toggle aria-controls='basic-navbar-nav'/> 
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='me-auto'>
-                            <Nav.Link href='#home'>Home</Nav.Link>
-                            <Nav.Link href='#home'>Service</Nav.Link>
-                            <Nav.Link href='#home'>Contact</Nav.Link>
-                        </Nav>
-                        <Form className='d-flex'>
-                            <FormControl type='Search' placeholder='Search' className='me-2'arial/>  
-                            <Button variant="outline-success">Search</Button> 
-                        </Form>
-                    </Navbar.Collapse> 
-                    <Button variant="primary" onClick={handleShow}>Regist</Button>          
-                </Container>
-            </Navbar>
             
-            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-                <ModalHeader closeButton>
-                    <ModalTitle>Modal Title</ModalTitle>
-                </ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <Form.Group className='md-3' controlId='formBasicText'>
-                          <Form.Control type="text" placeholder="Name" />
-                          <Form.Control type="text" placeholder="LastName" />
-                          <Form.Control type="number" placeholder="Age" />
-                        </Form.Group>
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                    <Button variant='secondary' onClick={handleClose}>Close</Button>
-                    <Button variant='success' > Enviar</Button>
-                </ModalFooter>
-            </Modal>
+            <header>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/Movie">About</Link>
+                </nav>
+            </header>
+
+            <main>
+                <Router>
+                    <Switch>
+                        <Route path=""></Route>
+                        <Route path=""></Route>
+                    </Switch>
+                </Router>
+                
+                <Button variant="primary" onClick={handleShow}>Regist</Button> 
+                <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                    <ModalHeader closeButton>
+                        <ModalTitle>Modal Title</ModalTitle>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <Form.Group className='md-3' controlId='formBasicText'>
+                            <Form.Control type="text" placeholder="Name" />
+                            <Form.Control type="text" placeholder="LastName" />
+                            <Form.Control type="number" placeholder="Age" />
+                            </Form.Group>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button variant='secondary' onClick={handleClose}>Close</Button>
+                        <Button variant='success' > Enviar</Button>
+                    </ModalFooter>
+                </Modal>
+            </main>
+
+            
         </>
     );
 };
